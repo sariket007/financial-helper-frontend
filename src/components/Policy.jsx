774963ccb022312f669be0a248af9b2d28f397cb
sariket007 <94927046+sariket007@users.noreply.github.com>
2026-03-19 23:41:12 +0530

    fix dynamic API url declaration to get it fetch from environment variable

--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -9,7 +9,7 @@ const Policy = () => {
     const fetchPolicies = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/policies/public",
+          `${import.meta.env.VITE_API_BASE_URL}/policies/public`,
         );
         setPolicies(response.data.data);
       } catch (error) {
